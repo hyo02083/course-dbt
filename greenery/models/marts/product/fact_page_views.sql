75f20398eb591dd0ad20fbaa293fc7b195a90be2
Jee Kim <hyo02083@gmail.com>
2024-10-25 13:31:48 +0000

    Add week3

--- greenery/models/marts/product/fact_page_views.sql
+++ greenery/models/marts/product/fact_page_views.sql
@@ -12,16 +12,33 @@ session_timing_agg as (
     select * from {{ ref('int_session_timings')}}
 )
 
+{% set event_types = [
+    'page_view',
+    'add_to_cart',
+    'checkout',
+    'package_shipped',
+]  %}
+
+-- Returns a list of the payment_methods in the stg_payments model_
+{% set event_types = dbt_utils.get_column_values(
+    table=ref('stg_postgres__events'),
+     column='event_type'
+) %}
+
+
+
+
 select
     e.session_id
     ,e.user_id
     ,coalesce(e.product_id, oi.product_id) as product_id
     ,session_started_at
     ,session_ended_at
-    ,sum(case when e.event_type = 'page_view' then 1 else 0 end ) as page_view
-    ,sum(case when e.event_type = 'add_to_cart' then 1 else 0 end ) as add_to_cart
-    ,sum(case when e.event_type = 'checkout' then 1 else 0 end ) as checkout
-    ,sum(case when e.event_type = 'package_shipped' then 1 else 0 end ) as package_shipped
+
+    {% for event_type in event_types %}
+    , {{ sum_of( 'e.event_type', event_type ) }} as {{event_type}}s 
+    {% endfor %}
+
     ,datediff('minute', session_started_at, session_ended_at) as session_length_minutes
 from events e
 left join order_items oi
---
+++ project_3.md
@@ -0,0 +1,27 @@
+# Part 1 
+## What is our overall conversion rate?  --구매 이벤트 세션 수 / 총 세션 수
+
+## What is our conversion rate by product? -- 작품별 구매 이벤트 세션 수 / 총 세션 수
+
+
+```
+# Part 2
+## Create a macro to simplify part of a model(s).
+- macros/sum_of.sql
+- models/marts/product/fact_page_views.sql
+
+
+# Part 3
+## Add a post hook to your project to apply grants to the role “reporting”. 
+
+# Part 4
+## Install a package (i.e. dbt-utils, dbt-expectations) and apply one or more of the macros to your project
+
+
+# Part 5
+## Show (using dbt docs and the model DAGs) how you have simplified or improved a DAG using macros and/or dbt packages.
+
+# Part 6
+## Run the products snapshot model using dbt snapshot and query it in snowflake to see how the data has changed since last week. 
+## Which products had their inventory change from week 2 to week 3? 
+
